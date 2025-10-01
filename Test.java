9887cab8b05a6dea8c6085286f81d1f7e1c5c722
ayuwardatulww-tech <ayuwardatulww@gmail.com>
2025-10-01 14:17:38 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,8 +1,8 @@
 public class Test {
     public static void main (Sting[] args){
-        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1");
-        System.out.println("user 1 menambah baris baru");
-        System.out.println("user 3 menambah baris baru");
+        System.out.println("Hi, ini baris kode pertama yang dimodifikasi user 1");//ayu
+        System.out.println("user 1 menambah baris baru");//ayu
+        System.out.println("user 3 menambah baris baru");//nahzul
 
     }
 }
